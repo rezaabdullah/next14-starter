1c7071d63462bc4c230e4deb8febb1a9c0996f16
rezaabdullah <reza@DESKTOP-C0JTLEI.localdomain>
2024-02-06 13:22:54 +0600

    feat: added features

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import NavBar from '@/components/NavBar/NavBar'
+import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <NavBar/>
+      {/* <body className={inter.className}>{children}</body> */}
+      {children}
+      <Footer/>
     </html>
   )
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Links from "./Links/Links";
+
+const NavBar = () => {
+    return (
+        <div>
+            <div>Logo</div>
+            {/* <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact Us</Link> */}
+            <Links></Links>
+        </div>
+    );
+  };
+  
+  export default NavBar;
