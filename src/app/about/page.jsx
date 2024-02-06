d4939ba8ca7ad4b7959e2dd9db6652db45ef2497
rezaabdullah <reza@DESKTOP-C0JTLEI.localdomain>
2024-02-07 01:57:22 +0600

    style: updated login on navmenu

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,9 +5,9 @@ const AboutPage = () => {
   return(
     <div className={styles.container}>
       <div className={styles.textContainer}>
-        <h2>About agency</h2>
-        <h1>We create unique customer experience</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        <h2 className={styles.subtitle}>About agency</h2>
+        <h1 className={styles.title}>We create unique customer experience</h1>
+        <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper.
           Aliquam faucibus purus in massa tempor nec feugiat.</p>
         <div className={styles.boxes}>
@@ -15,12 +15,19 @@ const AboutPage = () => {
             <h1>6+</h1>
             <p>Years of experience</p>
           </div>
-          {/* <div className={styles.box}></div>
-          <div className={styles.box}></div> */}
+          <div className={styles.box}>
+            <h1>10+</h1>
+            <p>Projects completed</p>
+          </div>
+          <div className={styles.box}>
+            <h1>250k+</h1>
+            <p>Revenue generated</p>
+          </div>
         </div>
       </div>
       <div className={styles.imgContainer}>
-        <Image src={"/about.png"} alt="" fill/>
+        {/* <Image src={"/about.png"} alt="" width={500} height={500} style={{width: "100%", height: "auto"}}/> */}
+        <Image src={"/about.png"} alt="" fill className={styles.img}/>
       </div>
     </div>
   );
