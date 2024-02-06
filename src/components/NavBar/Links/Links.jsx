7d6800f69bf1858b553db3180f2964229d1c5c90
rezaabdullah <reza@DESKTOP-C0JTLEI.localdomain>
2024-02-06 20:22:01 +0600

    style: updated style

--- src/components/NavBar/Links/Links.jsx
+++ src/components/NavBar/Links/Links.jsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+// import Link from "next/link";
 import styles from "./links.module.css"
 import NavLink from "./navLink/navLink";
 
@@ -22,12 +22,25 @@ const Links = () => {
         },
     ];
 
+    // TEMPORARY
+    const session = true;
+    const isAdmin = true;
+
     return (
         <div className={styles.links}>
             {links.map(link=>(
                 // <Link href={link.path} key={link.title}>{link.title}</Link>
                 <NavLink item={link} key={link.title}/>
-            ))}
+            ))}{
+                session ? (
+                    <>
+                        {isAdmin && <NavLink item={{title: "Admin", path: "/admin"}}/>}
+                        <button className={styles.logout}>Logout</button>
+                    </>
+                ) : (
+                    <NavLink item={{title: "Login", path: "/login"}}/>
+                )
+            }
         </div>
     );
   };
