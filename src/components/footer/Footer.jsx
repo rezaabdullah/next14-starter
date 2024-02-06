aa456102cbe04253b8fc2152066685b44e2877c0
rezaabdullah <reza@DESKTOP-C0JTLEI.localdomain>
2024-02-07 01:02:59 +0600

    feat: added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,8 @@
 const Footer = () => {
-    return <div>Footer</div>;
+    return <div className={styles.container}>
+      <div className={styles.logo}>Logo</div>
+      <div className={styles.text}>Abdullah Reza Test Site © All rights reserved</div>
+    </div>;
   };
   
   export default Footer;
