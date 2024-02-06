827f9de083d1122acdc959af3affae8bf79c38da
rezaabdullah <reza@DESKTOP-C0JTLEI.localdomain>
2024-02-07 00:22:14 +0600

    style: added link to logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import Links from "./Links/Links";
 import styles from "./navbar.module.css"
 
 const NavBar = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.logo}>Logo</div>
+            <Link href="/" className={styles.logo}>Logo</Link>
             <div>
                 <Links></Links>
             </div>
