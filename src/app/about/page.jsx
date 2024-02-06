aabaf4d889cf545bf78bf1e994ad523f0f49a10a
rezaabdullah <reza@DESKTOP-C0JTLEI.localdomain>
2024-02-07 01:22:10 +0600

    feat: added about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,25 @@ import styles from "./about.module.css";
 
 const AboutPage = () => {
   return(
-    <div className={styles.imgContainer}>
-      {/* <Image src={"/about.png"} alt="" fill/> */}
-      <Image src={"https://images.pexels.com/photos/3862615/pexels-photo-3862615.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"} alt="" fill/>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2>About agency</h2>
+        <h1>We create unique customer experience</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper.
+          Aliquam faucibus purus in massa tempor nec feugiat.</p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>6+</h1>
+            <p>Years of experience</p>
+          </div>
+          {/* <div className={styles.box}></div>
+          <div className={styles.box}></div> */}
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src={"/about.png"} alt="" fill/>
+      </div>
     </div>
   );
 };
