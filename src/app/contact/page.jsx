f7875430f47bcd3ed2b250c475bc817821d7f909
rezaabdullah <reza@DESKTOP-C0JTLEI.localdomain>
2024-02-07 02:51:52 +0600

    style: contactpage complete

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,27 @@
+import Image from "next/image";
+import styles from "./contactpage.module.css"
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return <div className={styles.container}>
+    <div className={styles.imgContainer}>
+      <Image src="/contact.png" alt="" fill className={styles.img}></Image>
+    </div>
+    <div className={styles.formContainer}>
+      <form action="" className={styles.form}>
+        <input type="text" placeholder="Enter full name"></input>
+        <input type="text" placeholder="Enter email address"></input>
+        <input type="text" placeholder="Enter phone number"></input>
+        <textarea
+          name=""
+          id=""
+          cols={30}
+          rows={10}
+          placeholder="Message">
+        </textarea>
+        <button>Send</button>
+      </form>
+    </div>
+  </div>;
 };
 
 export default ContactPage;
