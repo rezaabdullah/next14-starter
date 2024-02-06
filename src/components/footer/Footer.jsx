7fbccf394eb2703c2a56fbdb6463545fea17e6dd
rezaabdullah <reza@DESKTOP-C0JTLEI.localdomain>
2024-02-07 01:06:44 +0600

    style: added footer styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,10 @@
+import styles from "./footer.module.css"
+
 const Footer = () => {
     return <div className={styles.container}>
       <div className={styles.logo}>Logo</div>
       <div className={styles.text}>Abdullah Reza Test Site © All rights reserved</div>
     </div>;
-  };
-  
-  export default Footer;
+};
+
+export default Footer;
