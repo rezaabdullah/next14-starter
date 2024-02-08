8248966c9a6e704ed05bdc7ecfe394c1479e9be2
rezaabdullah <reza@DESKTOP-C0JTLEI.localdomain>
2024-02-09 02:27:35 +0600

    chore: tested hydration error and solution

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,13 +1,35 @@
-"use client";
+"use client"; // disable SSR on specific compoment (hydrationTest.jsx)
 import Image from "next/image";
-import styles from "./contactpage.module.css"
+import styles from "./contactpage.module.css";
+// import { useEffect, useState } from "react"; disable SSR on specific compoment (hydrationTest.jsx)
+// import HydrationTest from "@/components/hydrationTest"; dynamic import for disabling SSR
+import dynamic from "next/dynamic";
+
+const HydrationTestNoSSR = dynamic(() => import("@/components/hydrationTest"), {ssr: false});
 
 const ContactPage = () => {
+  // hydration error explanation (ex1)
+  // SSR != CSR
+  // const a = Math.random();
+  // console.log(a);
+
+  // hydration error solution 1: useEffect (ex2)
+  // const[isClient, setIsClient] = useState(false);
+  // useEffect(() => {
+  //   setIsClient(true);
+  // }, [])
+  const a = Math.random();
+  console.log(a);
+
   return <div className={styles.container}>
     <div className={styles.imgContainer}>
       <Image src="/contact.png" alt="" fill className={styles.img}></Image>
     </div>
+    {/* {a}; ex1 */}
+    {/* {isClient && a}; ex2 */}
     <div className={styles.formContainer}>
+      {/* <HydrationTestNoSSR /> */}
+      <div suppressHydrationWarning>{a}</div>
       <form action="" className={styles.form}>
         <input type="text" placeholder="Enter full name"></input>
         <input type="text" placeholder="Enter email address"></input>
@@ -19,6 +41,7 @@ const ContactPage = () => {
           rows={10}
           placeholder="Message">
         </textarea>
+        {/* <button onClick={() => console.log("clicked")}>Send</button> */}
         <button>Send</button>
       </form>
     </div>
